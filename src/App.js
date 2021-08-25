2a808efcd99e57ccfd6bb9d02cc247d9b95e5be0
Sam Cook <sam.cook@suusuccess.org>
2021-08-25 14:50:24 -0600

    final changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
-
+import {useState, useEffect} from 'react';
+import Player from './components/Player';
 
 function App() {
+  const [songs] = useState([
+    {
+      title: "Desole",
+      artist: "Gorillaz",
+      img_src: "./images/songmachine.jpg",
+      src: "./music/Gorillaz - Désolé ft. Fatoumata Diawara (Official Lyric Video).mp3"
+    },
+    {
+      title: "Clint Eastwood",
+      artist: "Gorillaz",
+      img_src: "./images/clinteastwood.jpg",
+      src: "./music/Clint Eastwood.mp3"
+    },
+    {
+      title: "Feel Good Inc.",
+      artist: "Gorillaz",
+      img_src: "./images/demondays.jpg",
+      src: "./music/Gorillaz - Feel Good Inc HD.mp3"
+    },
+  ])
+
+  const [
+    currentSongIndex, setCurrentSongIndex
+  ] = useState(0);
+
+  const [
+    nextSongIndex, setNextSongIndex
+  ] = useState(currentSongIndex + 1);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    })
+  }, [currentSongIndex]);
+
   return (
     <div className="App">
-      App
+      <Player currentSongIndex={currentSongIndex}
+      setCurrentSongIndex={setCurrentSongIndex}
+      nextSongIndex={nextSongIndex}
+      songs={songs} />
     </div>
   );
 }
